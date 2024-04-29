be3b5848b8edddb109e025721c6b2d21240c0aa3
alexmdrake <alexmdrake7@gmail.com>
2024-04-29 00:14:36 -0400

    change to pointers

--- upcxx.hpp
+++ upcxx.hpp
@@ -28,11 +28,12 @@ int upcxx_astar(int grid_size, std::vector<Obstacle> obstacleList){//, Point sta
 
     upcxx::global_ptr<std::unordered_set<Node*, NodeHash, NodeEqual>> closed_set 
     =upcxx::broadcast(upcxx::new_<std::unordered_set<Node*, NodeHash, NodeEqual>>(),0).wait() ;
+    std::unordered_map<Node, Node, NodeHash, NodeEqual> node_to_parent;
 
-    if(upcxx::rank_me()==0)(*local_queue).push(map.start);
+    if(upcxx::rank_me()==0)(*local_queue).push(new Node(map.startX, map.startY));
 
     std::cout << map.startX << " " << map.startY << std::endl;
-    std::cout << map.goalX << " " << map.goalY << std::endl;
+    std::cout << map.endX << " " << map.endY << std::endl;
     upcxx::global_ptr<bool>path_found = upcxx::broadcast(upcxx::new_<bool>(false),0).wait();
     Node* end_node;
     while((*local_queue).size() > 0){
@@ -45,10 +46,10 @@ int upcxx_astar(int grid_size, std::vector<Obstacle> obstacleList){//, Point sta
 	std::unordered_set<Node*, NodeHash, NodeEqual> set = upcxx::rget(closed_set).wait();
 	set.emplace(cur); 
 	upcxx::rput(set, closed_set).wait(); 
-        map.close_node(cur.x, cur.y);
+        map.close_node(cur->x, cur->y);
 
         if(*cur == *(map.goal)){
-		upcxx::rput(true,path_found).wait();
+		    upcxx::rput(true,path_found).wait();
             end_node = cur;
             break;
         }
@@ -59,33 +60,37 @@ int upcxx_astar(int grid_size, std::vector<Obstacle> obstacleList){//, Point sta
             if(upcxx::rget(closed_set).wait().find(n) != upcxx::rget(closed_set).wait().end() || !map.is_valid_node(*n)){
                 continue;
             }
-            n->cost_to_come = cur->cost_to_come + 1;
-            n->heuristic_cost = n->cost_to_come + n->heuristic(*(map.goal));
-            n->parent = cur;
-	    upcxx::rpc((upcxx::rank_me())%upcxx::rank_n(), local_insert,local_queue, n, 0,(*local_queue).size()).wait(); 
-	    map.open_node(n.x, n.y);
+            n.cost_to_come = cur.cost_to_come + 1;
+            n.heuristic_cost = n.cost_to_come + n.heuristic(Node(map.endX, map.endY));
+            Node new_parent = Node(cur.x, cur.y);
+            new_parent.cost_to_come = cur.cost_to_come;
+            new_parent.heuristic_cost = cur.heuristic_cost;
+            node_to_parent[n] = new_parent;
+	        upcxx::rpc((upcxx::rank_me())%upcxx::rank_n(), local_insert,local_queue, n, 0,(*local_queue).size()).wait(); 
+	        map.open_node(n.x, n.y);
         }
     }
 
     if(upcxx::rget(path_found).wait()){
         Node* cur = end_node;
         while(cur != nullptr && cur->parent != nullptr){
-            cur = cur->parent;
+            Node parent = node_to_parent[*cur];
             map.add_to_path(cur.x, cur.y);
+            cur = parent;
         }
     }
 
     for (auto it = upcxx::rget(closed_set).wait().begin(); it != upcxx::rget(closed_set).wait().end(); ++it) {
         delete *it;
     }
-    std::unordered_set<Node*, NodeHash, NodeEqual> set = upcxx::rget(closed_set).wait();
+    /*std::unordered_set<Node*, NodeHash, NodeEqual> set = upcxx::rget(closed_set).wait();
     set.clear(); 
     upcxx::rput(set, closed_set).wait(); 
     while (!(*local_queue).empty()) {
         Node *n = (*local_queue).top();
         (*local_queue).pop();
         delete n;
-    }
+    }*/
 
     map.render();
     upcxx::finalize(); 
